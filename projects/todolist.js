7b206235c8cc03bfff098559d486c3d41ee27538
Henry Pan <hanxiangp@gmail.com>
2014-11-20 10:52:37 -0500

    second commit todolist
    updated item count feature
    improved ui

--- projects/todolist.js
+++ projects/todolist.js
@@ -11,8 +11,7 @@ function addItem() {
 
 }
 
-function deleteContent() {
-	// document.getElementById("item").innerHTML = "";
+function deleteItem() {
 	document.getElementById('itemList').removeChild(document.getElementById('itemList').lastChild);
 	
 }
@@ -26,11 +25,24 @@ function unTest() {
 }
 
 function testing () {
-var x = document.getElementById("itemInput");
-var text = "";
-var i;
-for (i = 0; i < x.length; i++) {
-    text += x.elements[i].value + "<br>";
+	var x = document.getElementById("itemInput");
+	var text = "";
+	var i;
+	for (i = 0; i < x.length; i++) {
+		text += x.elements[i].value + "<br>";
+	}
+	document.getElementById("demo").innerHTML = text;
 }
-document.getElementById("demo").innerHTML = text;
+
+function numItems () {
+	var w = document.getElementById('itemList');
+	var count = 0; // this will contain the total elements.
+	for (var i = 0; i < w.childNodes.length; i++) {
+		var node = w.childNodes[i];
+		if (node.nodeName == "TR") {
+			count++;
+		}
+	}
+	
+	document.getElementById("itemCount").innerHTML = count + " items";
 }
